0d6b828ebf3481342ef544b6261366d4cd70c4ba
LoreSqueri <91483152+LoreSqueri@users.noreply.github.com>
2021-11-30 10:33:15 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,9 @@
+//
+// Created by squer on 29/11/2021.
+//
 
+#ifndef INC_7_NUMERI_PRIMI_LORESQUERI_LIB_H
+#define INC_7_NUMERI_PRIMI_LORESQUERI_LIB_H
+
+#endif //INC_7_NUMERI_PRIMI_LORESQUERI_LIB_H
+bool numerip (int, int);
