b2f78d6290f83f4327cff62aecfdb236544a2257
LoreSqueri <91483152+LoreSqueri@users.noreply.github.com>
2021-11-30 10:34:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+using namespace std;
+int main() {
+    int n=0;
+    cin >> n;
+    if (numerip(n ,n-1)){
+        cout << "numero primo";
+    }else{
+        cout << "numero non primo";
+    }
+    return 0;
 }
+
