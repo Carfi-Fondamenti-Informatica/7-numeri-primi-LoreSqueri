267687df33a7b330422d73f80261ea8640e8cc8b
LoreSqueri <91483152+LoreSqueri@users.noreply.github.com>
2021-11-30 10:32:07 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+//
+// Created by squer on 29/11/2021.
+//
 
+#include "lib.h"
+bool numerip (int n, int a){
+    if (n<0){
+        return false;
+    }else if (n==1){
+        return true;
+    }else if (a==1) {
+        return true;
+    }else if (n%a==0){
+        return false;
+    }
+    numerip(n,--a);
+}
